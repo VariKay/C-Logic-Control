639261c99ca80c691c93dcb83eadd8ba7fd22f60
Farai Mathemera <mathemerafarai@gmail.com>
2020-12-08 09:39:15 +0200

    Add files via upload

---
+++ Loops/Print Numbers/sum_of_numbers.cpp
@@ -0,0 +1,2 @@
+// C++ program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
---
+++ Loops/Print Numbers/sum_of_positive_number.cpp
@@ -0,0 +1,3 @@
+// program to find the sum of positive numbers
+// If the user enters a negative number, the loop ends
+// the negative number entered is not added to the sum
