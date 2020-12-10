a8172ab68ea193597e3afa1c7084cd9b8678a638
VariKay <75139107+VariKay@users.noreply.github.com>
2020-12-10 09:41:29 +0200

    Code to  print  numbers

--- Loops/Print Numbers/print_i_to_n.cpp
+++ Loops/Print Numbers/print_i_to_n.cpp
@@ -1,1 +1,11 @@
 // C++ Program to print numbers from 1 to n
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+    }
+    return 0;
+}
